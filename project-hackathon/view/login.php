addcb4102a2530cbf23acccaabc95f8d8620a5c5
daviqueir <daviqueiroz754@gmail.com>
2020-06-14 12:27:42 -0300

    login screen

---
+++ project-hackathon/view/login.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php include('styles.php');?>
+
+
+	<title>Home</title>
+</head>
+<body class="login-background">
+
+
+
+<div class="container">
+	
+	<header class="sticky header-properties">
+	<?php include('header.php');?>
+</header>
+
+<div class="row">
+	
+	<div class="col-sm-4 col-md-12 col-lg-12">
+
+			<center>
+
+		<div >
+
+			<img class="login-image" src="https://placehold.it/800x600">
+
+	</div>
+
+			</center>
+
+
+</div>
+
+</div>
+
+
+<div class="row">
+	
+	<div class="col-sm-12 col-md-12 col-lg-12">
+
+		<div class="form-login">
+
+			<div class="input-group fluid ">
+  <input class="background-form-login" type="text" id="Username" placeholder="Username or Email"/>
+</div>
+<div class="input-group vertical">
+  <input class="background-form-login" type="text" id="Username" placeholder="Enter Password"/>
+</div>
+
+
+	</div>
+
+</div>
+
+</div>
+
+<div class="row margin-login-options">
+
+	<div class="col-sm-12">
+
+		<center>
+			<div  >
+		 <p class="create-account-text">Criar conta</p>
+		</div>
+		  </center>
+
+	</div>
+
+</div>
+
+<div class="row margin-login-options">
+
+	<div class="col-sm-12">
+
+			<div class="solid-border-login email-login-properties">
+				<div >
+				<span class="icon-mail name-and-icon-login"></span>
+		 <p class="name-and-icon-login">LOGIN COM E-MAIL</p>
+		</div>
+	</div>
+
+	</div>
+
+</div>
+
+<div class="row margin-login-options">
+
+	<div class="col-sm-12">
+
+		<center>
+			<div class="solid-border-login">
+		 <p class="other-option-login">OUTRAS OPÇÔES</p>
+		</div>
+		  </center>
+
+	</div>
+
+</div>
+
+
+
+
+
+<head>
+
+<style type="text/css">
+	
+
+</style>
+
+</head>
+
+
+<footer class="sticky">
+<?php include('footer.php')?>
+</footer>
+
+
+</div>
+
+
+</body>
+</html>
