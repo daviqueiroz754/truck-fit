e1f0de835961377d7fbb66a4c062424b750e0c55
daviqueir <daviqueiroz754@gmail.com>
2020-06-14 08:44:49 -0300

    first commit

---
+++ project-hackathon/home.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
+
+
+	<title>Home</title>
+</head>
+<body>
+
+<div class="container">
+
+<div class="row">
+	
+	<div class="col-sm-12">
+
+		<p>Hello!</p>
+
+
+	</div>
+
+</div>
+
+
+<div class="row">
+	
+	<div class="col-sm-12">
+
+		<p>Hello!</p>
+
+
+	</div>
+
+</div>
+
+<div class="row">
+	
+	<div class="col-sm-12">
+
+		<p>Hello!</p>
+
+
+	</div>
+
+</div>
+
+</div>
+
+
+
+</body>
+</html>
