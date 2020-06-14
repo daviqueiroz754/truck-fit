92f6f1824eabf994a7b418984be3b1cc4f6e8b44
daviqueir <daviqueiroz754@gmail.com>
2020-06-14 09:20:03 -0300

    styles module added

---
+++ project-hackathon/home.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?=include('styles.php');?>
+
+
+	<title>Home</title>
+</head>
+<body>
+
+<div class="container">
+
+<div class="row">
+	
+	<div class="col-sm-12">
+
+		<div>
+
+			<h1>Hello</h1>
+
+
+	</div>
+
+</div>
+
+
+<div class="row">
+	
+	<div class="col-sm-12">
+
+		<div>
+
+						<h1>Hello</h1>
+
+			
+		</div>
+
+
+	</div>
+
+</div>
+
+<div class="row">
+	
+	<div class="col-sm-12">
+
+		<div>
+
+						<h1>Hello</h1>
+
+			
+		</div>
+
+
+	</div>
+
+</div>
+
+</div>
+
+
+
+</body>
+</html>
